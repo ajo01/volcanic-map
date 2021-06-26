5372a6a54f4d5031c2df1beff6d2a590abc6426d
ajo01 <amyjojjm2001@gmail.com>
2021-06-26 14:00:39 -0700

    layer control panel

--- map.py
+++ map.py
@@ -27,17 +27,24 @@ def color_producer(elev):
 map = folium.Map(location=[38.58, -99.09],
                  zoom_start=6, tiles="Stamen Terrain")
 
-fg = folium.FeatureGroup(name="My Map")
+fg_vol = folium.FeatureGroup(name="Volcanoes")
 
 for lt, ln, el, nm in zip(lat, lon, elev, name):
     iframe = folium.IFrame(html=html % (
         nm, nm, str(el)), width=200, height=100)
-    fg.add_child(folium.CircleMarker(location=[lt, ln], popup=folium.Popup(
+    fg_vol.add_child(folium.CircleMarker(location=[lt, ln], popup=folium.Popup(
         iframe), radius=8, fill_color=color_producer(el), color='grey', fill_opacity=0.7))
 
-fg.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='utf-8-sig').read(),
+
+fg_pop = folium.FeatureGroup(name="Population")
+
+fg_pop.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='utf-8-sig').read(),
 style_function=lambda x: {'fillColor': 'green' if x['properties']['POP2005'] < 10000000 
 else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
 
-map.add_child(fg)
+
+
+map.add_child(fg_vol)
+map.add_child(fg_pop)
+map.add_child(folium.LayerControl())
 map.save("map1.html")
