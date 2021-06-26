93cb25698c103c48a9cada6adb01c6bffad2820d
ajo01 <amyjojjm2001@gmail.com>
2021-06-26 12:19:10 -0700

    all volcano markers show

--- map.py
+++ map.py
@@ -1,7 +1,18 @@
 import folium
+import pandas
+
+data = pandas.read_csv("Volcanoes.txt")
+lat = list(data["LAT"])
+lon = list(data["LON"])
+
 map = folium.Map(location=[38.58, -99.09],
                  zoom_start=6, tiles="Stamen Terrain")
-map.add_child(folium.Marker(
-    location=[38.2, -99.1], popup="marker", icon=folium.Icon(color='green')))
 
+fg = folium.FeatureGroup(name="My Map")
+
+for lt, ln in zip(lat, lon):
+    fg.add_child(folium.Marker(
+        location=[lt, ln], popup="marker", icon=folium.Icon(color='green')))
+
+map.add_child(fg)
 map.save("map1.html")
