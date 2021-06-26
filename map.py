b7f6e256d8bb86b276d5c457e654e257d33bf8b0
ajo01 <amyjojjm2001@gmail.com>
2021-06-26 13:07:26 -0700

    marker cards show volcanic info

--- map.py
+++ map.py
@@ -6,14 +6,18 @@ lat = list(data["LAT"])
 lon = list(data["LON"])
 elev = list(data["ELEV"])
 
+
+html = """<h4>Volcano information:</h4>
+Height: %sm
+"""
+
 map = folium.Map(location=[38.58, -99.09],
                  zoom_start=6, tiles="Stamen Terrain")
 
 fg = folium.FeatureGroup(name="My Map")
 
 for lt, ln, el in zip(lat, lon, elev):
-    fg.add_child(folium.Marker(
-        location=[lt, ln], popup=str(el) + "m", icon=folium.Icon(color='green')))
-
+    iframe = folium.IFrame(html=html % str(el), width=200, height=100)
+    fg.add_child(folium.Marker(location=[lt, ln], popup=folium.Popup(iframe), icon = folium.Icon(color = "green")))
 map.add_child(fg)
 map.save("map1.html")
